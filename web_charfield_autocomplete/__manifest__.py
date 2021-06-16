6e32c69f07a74a9883bab721fef183b27bb3b8b6
Hai Lang <hailn@trobz.com>
2021-06-16 08:11:28 +0700

    [IMP] web_charfield_autocomplete: black, isort, prettier

--- web_charfield_autocomplete/__manifest__.py
+++ web_charfield_autocomplete/__manifest__.py
@@ -1,21 +1,17 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2016 Camptocamp Simone Orsi <simone.orsi@camptocamp.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Char field autocomplete widget',
-    'summary': """Add a new configurable widget for char fields.""",
-    'version': '14.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': (
-        'Camptocamp SA,'
-        'Odoo Community Association (OCA)'
-    ),
-    'website': 'http://camptocamp.com',
-    'depends': [
-        'web',
+    "name": "Char field autocomplete widget",
+    "summary": """Add a new configurable widget for char fields.""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": ("Camptocamp SA," "Odoo Community Association (OCA)"),
+    "website": "https://github.com/OCA/web",
+    "depends": [
+        "web",
     ],
-    'data': [
-        'templates/assets.xml',
+    "data": [
+        "templates/assets.xml",
     ],
 }
